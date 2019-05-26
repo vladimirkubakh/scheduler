2e6364a8d5012e221752b4dab5c036b663c16c2c
Vladimir Kubakh <refrain14@list.ru>
2019-05-26 22:44:01 +0300

    конструктор

--- src/Component/App.js
+++ src/Component/App.js
@@ -34,6 +34,35 @@ const HOST = PRODUCTION ? '/' : 'http://localhost:3000/'
 
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: true,
+      navOpen: false,
+      confirmationModalOpen: false,
+      confirmationTextVisible: false,
+      stepIndex: 0,
+      appointmentDateSelected: false,
+      appointmentMeridiem: 0,
+      validEmail: true,
+      validPhone: true,
+      smallScreen: window.innerWidth < 768,
+      confirmationSnackbarOpen: false
+    }
 
+    this.handleNavToggle = this.handleNavToggle.bind(this)
+    this.handleNextStep = this.handleNextStep.bind(this)
+    this.handleSetAppointmentDate = this.handleSetAppointmentDate.bind(this)
+    this.handleSetAppointmentSlot = this.handleSetAppointmentSlot.bind(this)
+    this.handleSetAppointmentMeridiem = this.handleSetAppointmentMeridiem.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.validateEmail = this.validateEmail.bind(this)
+    this.validatePhone = this.validatePhone.bind(this)
+    this.checkDisableDate = this.checkDisableDate.bind(this)
+    this.renderAppointmentTimes = this.renderAppointmentTimes.bind(this)
+    this.renderConfirmationString = this.renderConfirmationString.bind(this)
+    this.renderAppointmentConfirmation = this.renderAppointmentConfirmation.bind(this)
+    this.resize = this.resize.bind(this)
+  }
 
 }
