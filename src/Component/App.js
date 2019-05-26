a889beb229049e251dceb07616960da11ba2f042
Vladimir Kubakh <refrain14@list.ru>
2019-05-26 22:42:07 +0300

    импорты

---
+++ src/Component/App.js
@@ -0,0 +1,30 @@
+import { Component } from 'react'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import axios from 'axios'
+import async from 'async'
+import moment from 'moment'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import Dialog from 'material-ui/Dialog'
+import Divider from 'material-ui/Divider'
+import MenuItem from 'material-ui/MenuItem'
+import Card from 'material-ui/Card'
+import DatePicker from 'material-ui/DatePicker'
+import TimePicker from 'material-ui/TimePicker'
+import TextField from 'material-ui/TextField'
+import SelectField from 'material-ui/SelectField'
+import SnackBar from 'material-ui/Snackbar'
+import {
+  Step,
+  Stepper,
+  StepLabel,
+  StepContent,
+  StepButton
+} from 'material-ui/stepper'
+import {
+  RadioButton,
+  RadioButtonGroup
+} from 'material-ui/RadioButton'
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton'
+import logo from './../../assets/images/logo.svg'
