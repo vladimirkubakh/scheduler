f96ecdba81cac89eb6342263c3cb745c9e8c34ec
Vladimir Kubakh <refrain14@list.ru>
2019-05-26 22:46:06 +0300

    два валидатора

--- src/Component/App.js
+++ src/Component/App.js
@@ -138,5 +138,14 @@ export default class App extends Component {
       return this.setState({ confirmationSnackbarMessage: "Appointment failed to save.", confirmationSnackbarOpen: true })
     })
   }
+  validateEmail(email) {
+    const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    return regex.test(email) ? this.setState({ email: email, validEmail: true }) : this.setState({ validEmail: false })
+  }
+
+  validatePhone(phoneNumber) {
+    const regex = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/
+    return regex.test(phoneNumber) ? this.setState({ phone: phoneNumber, validPhone: true }) : this.setState({ validPhone: false })
+  }
 
 }
